021bcdd4ca9e89a93a0688b6b07f24b0e5f7a4b7
kuri-sun <harukikuriwada@gmail.com>
2025-10-09 00:52:57 +0900

    chore: splitting misc -> snacks.lua

---
+++ lua/yoda/plugins/snacks.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+function M.highlights(colors, set)
+	-- Snacks.nvim highlights
+	set(0, "SnacksNormal", { fg = colors.fg, bg = colors.bg })
+	set(0, "SnacksBorder", { fg = colors.border })
+	set(0, "SnacksTitle", { fg = colors.bright })
+	set(0, "SnacksFooter", { fg = colors.dim })
+	set(0, "SnacksKey", { fg = colors.bright })
+	set(0, "SnacksValue", { fg = colors.fg })
+	set(0, "SnacksDesc", { fg = colors.dim })
+	set(0, "SnacksGroup", { fg = colors.bright })
+	
+	-- Snacks file search character match highlights
+	set(0, "SnacksPickerMatch", { fg = colors.accent })
+	set(0, "SnacksPickerCursor", { fg = colors.accent, bg = colors.selection })
+	set(0, "SnacksSearchMatch", { fg = colors.accent })
+end
+
+return M
